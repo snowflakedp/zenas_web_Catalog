b5bfbf3accee2a0cb550164f7b789c191756138b
snowflakedp <110241849+snowflakedp@users.noreply.github.com>
2022-08-05 10:49:04 -0400

    Update catalog_app.py

--- catalog_app.py
+++ catalog_app.py
@@ -12,12 +12,11 @@ my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from zenas_athleisure_db.products.catalog_for_website")
 my_catalog = my_cur.fetchall()
-streamlit.stop()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
 
-
+streamlit.stop()
 
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
