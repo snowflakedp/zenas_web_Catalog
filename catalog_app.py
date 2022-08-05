8417acd12b8fcba94871e7c6057cc42ba2dd1239
snowflakedp <110241849+snowflakedp@users.noreply.github.com>
2022-08-05 10:54:44 -0400

    Update catalog_app.py

--- catalog_app.py
+++ catalog_app.py
@@ -31,8 +31,6 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from zenas_athleisure_db.products.catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 
---streamlit.stop()
-
 streamlit.image(
           df2[0],
           width=400,
