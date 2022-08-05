ffd15762604efdafdeab13c9508a27c5bb9a4ddb
snowflakedp <110241849+snowflakedp@users.noreply.github.com>
2022-08-05 10:48:38 -0400

    Update catalog_app.py

--- catalog_app.py
+++ catalog_app.py
@@ -7,11 +7,12 @@ streamlit.title('My Parents New Healthy Diner')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-streamlit.stop()
+
 
 # run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
+my_cur.execute("select color_or_style from zenas_athleisure_db.products.catalog_for_website")
 my_catalog = my_cur.fetchall()
+streamlit.stop()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
